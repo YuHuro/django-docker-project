b436996e36c3549d47631edb7e29843ddd4677b7
Yo <ayubovmagomed999@gmail.com>
2025-03-20 15:00:33 +0300

    Добавлено перенаправление на меню

--- portfolioProject/urls.py
+++ portfolioProject/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 
 
 urlpatterns = [
-    path('', lambda request: redirect('menu:menu', permanent=True)),
+    path('', RedirectView.as_view(url=reverse_lazy('menu:menu'), permanent=True)),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('menu/', include('menu.urls')),
