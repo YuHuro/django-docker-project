4c1688cf4268d8c56135f693cebcbad431c4551a
Yo <ayubovmagomed999@gmail.com>
2025-03-20 14:52:07 +0300

    Добавлено перенаправление на меню

--- menu/urls.py
+++ menu/urls.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from .views import menu_view, add_to_cart
 
 urlpatterns = [
-    path('/menu/', lambda request: redirect('/menu/', permanent=True)),
+    path('', lambda request: redirect('/menu/', permanent=True)),
     path('menu/', menu_view, name='menu'),
     path('add-to-cart/<int:dish_id>/', add_to_cart, name='add_to_cart'),
 ]
