bb5bf63c5c4dd32850dd7fb0c950df0558778e71
Yo <ayubovmagomed999@gmail.com>
2025-03-20 15:36:06 +0300

    Добавлено перенаправление на меню

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import menu_view, add_to_cart
 
 urlpatterns = [
-    path('menu/', menu_view, name='menu'),
+    path('', menu_view, name='menu'),
     path('add-to-cart/<int:dish_id>/', add_to_cart, name='add_to_cart'),
 ]
